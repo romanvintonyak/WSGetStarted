40e61e32a33839ff23f611da9f5a78286261836e
Roman Vintonyak <roma.vintonyak@gmail.com>
2015-05-22 01:43:55 +0300

    not woking

--- src/main/java/com/osf/romanvintonyak/WSDummy/dao/UserDao.java
+++ src/main/java/com/osf/romanvintonyak/WSDummy/dao/UserDao.java
@@ -4,19 +4,23 @@ import com.osf.romanvintonyak.WSDummy.Entities.User;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 
 /**
  * Created by Roman on 21.05.2015.
  */
-@Stateless
+
 public class UserDao {
-    @PersistenceContext(unitName="primary")
-    private EntityManager em;
 
     public User findById(long id) {
-        System.out.println(em);
-        return em.find(User.class, id);
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MyPU");
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        User user = em.find(User.class, id);
+        System.out.println(user);
+        em.getTransaction().commit();
+        em.close();
+
+        return user;
     }
 }
